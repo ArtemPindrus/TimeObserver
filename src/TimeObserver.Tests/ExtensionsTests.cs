5671dcf941e6872bf5ce80978d19cdda2422e68f
Artem Pindrus <artempindrus@gmail.com>
2025-07-20 15:47:52 +0300

    project restructure

---
+++ src/TimeObserver.Tests/ExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeObserver.Extensions;
+
+namespace TimeObserver.Tests {
+    public class ExtensionsTests {
+        [Fact]
+        private void SeparateByUppercaseValid() {
+            string input = "HelloWorldThere";
+            string expected = "Hello World There";
+            string result = input.SeparateByUppercase();
+            Assert.Equal(expected, result);
+        }
+    }
+}
